50699e793e0a6f3e2ce303c1fcfd9478f762010e
MJ HUghes <mhughes21@my.wctc.edu>
2019-03-15 11:06:08 -0500

    Blog display and add
    1) Add user menu
    2) Move code to display all blogs to displayAllBlogs() method
    3) Move code to add a new blog to addNewBlog() method

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -13,25 +13,31 @@ namespace BlogsConsole
             logger.Info("Program started");
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                string choice = "";
+                do
                 {
-                    Console.WriteLine(item.Name);
-                }
+                    Console.WriteLine("");
+                    Console.WriteLine("1) Display all blogs.");
+                    Console.WriteLine("2) Add a new blog.");
+                    Console.WriteLine("3) Create a post.");
+                    Console.WriteLine("Press any other key to exit.");
+                    // input response
+                    choice = Console.ReadLine();
+                    if (choice == "1")
+                    {
+                        displayAllBlogs();
+                    }
+                    else if (choice == "2")
+                    {
+                        addNewBlog();                        
+                    }
+                    if (choice == "3")
+                    {
+                        
+                    }
+
+                } while (choice == "1" || choice == "2" || choice == "3");
+
             }
             catch (Exception ex)
             {
@@ -43,5 +49,45 @@ namespace BlogsConsole
 
             logger.Info("Program ended");
         }
+
+        public static void displayAllBlogs()
+        {
+            var db = new BloggingContext();
+
+            // Display all Blogs from the database
+            var query = db.Blogs.OrderBy(b => b.Name);
+
+            Console.WriteLine("All blogs in the database:");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void addNewBlog()
+        {
+            // Create and save a new Blog
+            Boolean done = false;
+            while (!done)
+            {
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+                if (name.Length>2)
+                {
+                    Console.WriteLine("Blog name must be at least two characters.");
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            var blog = new Blog { Name = name };
+            var db = new BloggingContext();
+
+            db.AddBlog(blog);
+            logger.Info("Blog added - {name}", name);
+        }
+
     }
 }
